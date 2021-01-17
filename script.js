650d1b575b310937a10306cd16f38dda10a63563
biafarrugia <71879879+biafarrugia@users.noreply.github.com>
2021-01-17 16:51:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+/* ==== COUNTER ==== */
+
+var total = 15512; 
+var amountPerSecond = total/86400; 
+console.log("per second amount is "+amountPerSecond);
+var runningTotal = 0// set a running total which will change
+
+function updateAmount(){   console.log("updateAmount running");
+  console.log(runningTotal);
+ var integer = parseInt(runningTotal);
+   runningTotal = runningTotal+amountPerSecond;
+  document.getElementById('counter').innerHTML = integer;
+}
+setInterval(updateAmount, 1000); 
+
+/* ==== SCROLL REVEAL==== */
+ScrollReveal().reveal('.fold1', {delay: 100});
